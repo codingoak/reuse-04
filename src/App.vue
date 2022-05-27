426629e3a8feb31ba54738c51278ea5c12e87420
Daniel Eicher <d.eicher@mailbox.org>
2022-05-27 22:42:39 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <add-user></add-user>
+    <delete-user></delete-user>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddUser from "./components/AddUser.vue";
+import DeleteUser from "./components/DeleteUser.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    AddUser,
+    DeleteUser,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+main {
+  width: 40rem;
+  margin: 3rem auto;
+}
+
+section {
+  margin: 2rem auto;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+button {
+  font: inherit;
+  background-color: #310131;
+  border: 1px solid #310131;
+  border-radius: 8px;
+  color: white;
+  padding: 0.5rem 1.5rem;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #770e77;
+  border-color: #770e77;
 }
 </style>
